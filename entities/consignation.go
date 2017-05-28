ac3173addb7cd61c45a9935383f9d0fcbffacd00
william wei <1342247033@qq.com>
2017-05-29 00:54:34 +0800

    Implement BinaryMarshaler inteface

--- entities/consignation.go
+++ entities/consignation.go
@@ -2,6 +2,7 @@ package entities
 
 import (
 	"github.com/shopspring/decimal"
+	"strconv"
 )
 
 type Consignation struct {
@@ -12,4 +13,19 @@ type Consignation struct {
 	OpenQuantity int
 	Direction int
 	FirmId int
+}
+
+func (c Consignation) MarshalBinary() (data []byte, err error) {
+	return []byte(strconv.Itoa(c.Type) + "," +
+		strconv.Itoa(c.Quantity) + "," +
+		strconv.Itoa(c.FutureId) + "," +
+		strconv.Itoa(c.FutureId) + "," +
+		strconv.Itoa(c.OpenQuantity) + "," +
+		strconv.Itoa(c.Direction) + "," +
+		strconv.Itoa(c.FirmId) + "," +
+		c.Price.String() + ","),nil
+}
+
+func (c Consignation) UnmarshalBinary(data []byte) error  {
+	return nil
 }
